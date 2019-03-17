788fa6418053766a95aad0e669bd83eb9ca68485
Jess <jessica.barattini@gmail.com>
2019-03-17 15:54:06 -0500

    initial commit

---
+++ src/main/java/org/launchcode/cheesemvc2/CheeseMvc2Application.java
@@ -0,0 +1,13 @@
+package org.launchcode.cheesemvc2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheeseMvc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheeseMvc2Application.class, args);
+	}
+
+}
